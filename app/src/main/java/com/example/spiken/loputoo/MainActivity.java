2b2266e175a8d9bf44c6138e069cd0c034b5d184
Karlridala <karlobv1ously@gmail.com>
2018-06-20 17:28:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/spiken/loputoo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.spiken.loputoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.dicegame.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
